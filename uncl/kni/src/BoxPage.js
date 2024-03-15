996a80c249897346bfab45a3fd4ea281c10872a8
offline <vickymuthunga@gmail.com>
2024-03-15 15:56:02 +0300

    frontend

--- uncl/kni/src/BoxPage.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const BoxPage = () => {
-    return <div>This is a box page!</div>;
-};
-
-export default BoxPage;
--- uncl/kni/src/Message.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Messege = () => {
-    return <h2>welcome</h2>
-}
-
-export default Messege;
